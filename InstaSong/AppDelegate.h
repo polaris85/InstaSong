ca4c8463e5c6ed208ac586d32c65525db0d531a4
zongda liang <zongdaliang@173.44.51.20.static.quadranet.com>
2015-01-06 15:04:23 +0100

    Initial Commit

---
+++ InstaSong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstaSong
+//
+//  Created by zongda liang on 1/6/15.
+//  Copyright (c) 2015 com.liang.instasong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaSong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstaSong
+//
+//  Created by zongda liang on 1/6/15.
+//  Copyright (c) 2015 com.liang.instasong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
